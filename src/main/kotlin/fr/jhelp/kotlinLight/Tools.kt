048e14e76a00f4c5500254e4e8166c8459f8c783
gerardbourriaud <gerard.bourriaud@feetme.fr>
2020-10-07 14:57:47 +0200

    Fatal error

--- src/main/kotlin/fr/jhelp/kotlinLight/Tools.kt
+++ src/main/kotlin/fr/jhelp/kotlinLight/Tools.kt
@@ -5,3 +5,9 @@ internal fun <T, T1> Iterator<T>.transform(transformation: (T) -> T1) =
 
 internal fun <T, T1> Iterable<T>.transform(transformation: (T) -> T1) =
     TransformIterable(this, transformation)
+
+@Throws(RuntimeException::class)
+fun fatalError(message: String)
+{
+    throw RuntimeException("Fatal Error : $message")
+}
